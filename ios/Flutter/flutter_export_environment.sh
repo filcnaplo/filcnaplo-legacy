bed3ee1f9c2d78a367c29591ddda906e29113ce8
RedyAu <fodor.benedek2001@gmail.com>
2020-02-01 19:32:16 +0100

    New app icon
    Android and iOS as well (in theory at least)

--- ios/Flutter/flutter_export_environment.sh
+++ ios/Flutter/flutter_export_environment.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
-export "FLUTTER_ROOT=/opt/flutter"
-export "FLUTTER_APPLICATION_PATH=/home/marci/dev/filcnaplo"
-export "FLUTTER_TARGET=lib/main.dart"
+export "FLUTTER_ROOT=C:\Users\fodor\Documents\_Android\FlutterSDK\flutter"
+export "FLUTTER_APPLICATION_PATH=C:\Users\fodor\Documents\GitHub\filcnaplo"
+export "FLUTTER_TARGET=lib\main.dart"
 export "FLUTTER_BUILD_DIR=build"
-export "SYMROOT=${SOURCE_ROOT}/../build/ios"
-export "FLUTTER_FRAMEWORK_DIR=/opt/flutter/bin/cache/artifacts/engine/ios"
+export "SYMROOT=${SOURCE_ROOT}/../build\ios"
+export "FLUTTER_FRAMEWORK_DIR=C:\Users\fodor\Documents\_Android\FlutterSDK\flutter\bin\cache\artifacts\engine\ios"
 export "FLUTTER_BUILD_NAME=1.0.0"
 export "FLUTTER_BUILD_NUMBER=1"
