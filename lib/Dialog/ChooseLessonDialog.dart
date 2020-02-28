18788b3fdd6d4e5c03bbe975d0d0fddc6090dc67
RedyAu <fodor.benedek2001@gmail.com>
2020-02-28 12:15:50 +0100

    feat(ChooseLessonDialog) help choose lesson to add homework to

---
+++ lib/Dialog/ChooseLessonDialog.dart
@@ -0,0 +1,18 @@
+import 'package:filcnaplo/Helpers/RequestHelper.dart';
+import 'package:filcnaplo/generated/i18n.dart';
+import 'package:flutter/material.dart';
+import 'package:filcnaplo/Datas/Lesson.dart';
+import 'package:filcnaplo/globals.dart' as globals;
+import '../Utils/StringFormatter.dart';
+
+class ChooseLessonDialog extends StatefulWidget {
+
+  @override
+  ChooseLessonDialogState createState() => new ChooseLessonDialogState();
+}
+
+class ChooseLessonDialogState extends State<ChooseLessonDialog> {
+  Widget build(BuildContext context) {
+    return new Container();
+  }
+}
