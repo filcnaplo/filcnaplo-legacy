8e64be1ff8016b2bdbfb7664cbb4d4172ba5f74d
RedyAu <fodor.benedek2001@gmail.com>
2020-02-29 00:59:06 +0100

    nem vagyok hajlandó követni a commit guideot jajj de nagyon túl hosszú

--- lib/screens/homeworkScreen.dart
+++ lib/screens/homeworkScreen.dart
@@ -78,7 +78,7 @@ class HomeworkScreenState extends State<HomeworkScreen> {
                     });
                   },
                 ),
-                new IconButton(icon: Icon(Icons.plus_one), onPressed: _openChooser,)
+                //new IconButton(icon: Icon(Icons.plus_one), onPressed: _openChooser,)
               ],
             ),
             body: new Container(
