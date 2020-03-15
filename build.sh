7f47297af306341b13fcfab31b28a7814699197c
UnknownPlayer78 <nyitraiszabolcs4@gmail.com>
2020-03-15 22:04:00 +0100

    er

--- build.sh
+++ build.sh
@@ -1,20 +1,26 @@
 #!/bin/bash
 
 if [ "$1" == "-h" ]; then
-	echo "Usage: `basename $0` (--full)";
+	echo "Usage: `basename $0` [--clean]";
 	exit 0;
 fi
 
 inputFile=$(realpath $0)
 libDir=$(echo "$inputFile" | cut -f 1 -d '.')"/../lib"
+
+echo "Formatting code..."
 dartfmt -w --fix "$libDir";
 
-if [ "$2" == "--full" ]; then
-	echo "[FULL MODE ON!]"
+if [ "$2" == "--clean" ]; then
+	echo "Cleaning up..."
 	flutter clean;
+	echo "Uninstalling old version..."
 	adb shell pm uninstall hu.filcnaplo.ellenorzo;
 fi
 
+echo "Building app..."
 flutter build apk;
+
+echo "Installing app..."
 adb install -r build/app/outputs/apk/release/app-release.apk;
 flutter logs;
