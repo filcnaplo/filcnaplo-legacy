c26f410f5044f2829114cc077160ac1586dcb905
RedyAu <fodor.benedek2001@gmail.com>
2020-06-17 21:22:09 +0200

    Revert "action test - error"
    This reverts commit 1dfc49facc35f5f7fec4d8295aa5fb499faa6141.

--- lib/models/user.dart
+++ lib/models/user.dart
@@ -14,7 +14,6 @@ class User {
   Color color;
   Map<String, String> lastRefreshMap = Map();
   static const RATE_LIMIT_MINUTES = globals.rateLimitMinutes;
-  Ebből szép kis error lesz és az action majd sír
 
   User(this.id, this.username, this.password, this.name, this.schoolCode,
       this.schoolUrl, this.schoolName, this.parentName, this.parentId);
